90cf1ae62c53ca5862af24d15e8107e6e758a849
shashankdubey822-code <shashankdubey822@gmail.com>
2025-10-15 18:52:44 +0530

    Make app deployment-ready for hosting platforms

--- app.py
+++ app.py
@@ -202,6 +202,7 @@ def download_file(filename):
         return redirect(url_for('view_file', filename=filename, original_filename=metadata.get('original_filename', filename)))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
 
 
